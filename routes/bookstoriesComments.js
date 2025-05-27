4728c222555484e9683b3676186a0de159af5815
iyungui <rsdbddml@gmail.com>
2025-05-27 10:43:09 +0900

    [remove] unused importing functions

--- routes/bookstoriesComments.js
+++ routes/bookstoriesComments.js
@@ -7,13 +7,12 @@ const {
     getCommentsForBookStory,
     deleteComment,
     getCommentCountForBookStory
- } = require('../controllers/bookStoryCommentController');
+ } = require('../controllers/bookStoryCommentController'); // 파일명 수정
 
 router.post('/', ensureAuthenticated, addCommentToBookStory);
 
 router.get('/:bookStoryId', getCommentsForBookStory);
 router.get('/count/:bookStoryId', getCommentCountForBookStory);
 router.delete('/:commentId', ensureAuthenticated, deleteComment);
-
 
 module.exports = router;
