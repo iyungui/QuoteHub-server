0661ce6efc7411a78d32b207107a1e60699c61d4
iyungui <rsdbddml@gmail.com>
2025-08-02 00:19:50 +0900

    액세스토큰 만료 시간 1시간으로 설정

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ const auth = new AppleAuth(
 
 // JWT 토큰 생성 함수
 const generateAccessToken = (user) => {
-  return jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "1h" });
 };
 
 const generateRefreshToken = (user) => {
